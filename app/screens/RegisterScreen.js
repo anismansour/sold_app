5b221d76920ce82350d2e95600206182deeb3329
Anis Mansour <mansour.anis@hotmail.com>
2020-06-19 16:22:08 -0700

    register screen

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -3,24 +3,60 @@ import { StyleSheet, Text, View } from "react-native";
 import Screen from "../components/Screen";
 import AppTextInput from "../components/AppTextInput";
 import AppButton from "../components/AppButton";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import ErrorMessage from "../components/ErrorMessage";
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required().label("Name"),
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
 
 export default function RegisterScreen() {
   return (
     <Screen>
-      <AppTextInput icon="account" placeholder="Name" autoCorrect={false} />
-      <AppTextInput
-        icon="email"
-        placeholder="Email"
-        autoCorrect={false}
-        autoCapitalize="none"
-      />
-      <AppTextInput
-        icon="lock"
-        placeholder="Password"
-        autoCorrect={false}
-        autoCapitalize="none"
-      />
-      <AppButton title="REGISTER" color="red" />
+      <Formik
+        initialValues={{ name: "", email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        {({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
+          <>
+            <AppTextInput
+              icon="account"
+              placeholder="Name"
+              autoCorrect={false}
+              onChangeText={handleChange("name")}
+              onBlur={() => setFieldTouched("name")}
+            />
+            <ErrorMessage error={errors.name} visible={touched.name} />
+            <AppTextInput
+              icon="email"
+              placeholder="Email"
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={handleChange("email")}
+              onBlur={() => setFieldTouched("email")}
+            />
+            <ErrorMessage error={errors.email} visible={touched.email} />
+
+            <AppTextInput
+              icon="lock"
+              placeholder="Password"
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={handleChange("password")}
+              secureTextEntry={true}
+              textContentType="password"
+              onBlur={() => setFieldTouched("password")}
+            />
+            <ErrorMessage error={errors.password} visible={touched.password} />
+
+            <AppButton title="REGISTER" color="red" onPress={handleSubmit} />
+          </>
+        )}
+      </Formik>
     </Screen>
   );
 }
